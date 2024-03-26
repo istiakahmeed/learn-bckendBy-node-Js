a6a328ce782699280f29efc11f8ab4bdeb3bff60
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-26 15:32:09 +0600

    add input validation and create put and delete request

--- build-rest-api/index.js
+++ build-rest-api/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const Joi = require("joi");
 const courses = [
   { id: 1, name: "Course1" },
   { id: 2, name: "Course2" },
@@ -20,11 +20,57 @@ app.get("/api/courses", (req, res) => {
 //post /api/courses
 app.use(express.json());
 app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    res.status(400).send(error.message);
+    return;
+  }
   const course = {
     id: courses.length + 1,
     name: req.body.name,
   };
   courses.push(course);
+  res.send(course);
+});
+
+//update course
+
+app.put("/api/courses/:id", (req, res) => {
+  //Look up the courses
+  //if not exist, return 404
+  const id = parseInt(req.params.id);
+  const course = courses.find((c) => c.id === id);
+  if (!course) return res.status(404).send(`Course not found`);
+  //validate
+  const { error } = validateCourse(req.body);
+  //if valid, return 400 - bad request
+  if (error) {
+    res.status(400).send(error.message);
+    return;
+  }
+  //return the updated course
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(course);
+}
+
+//delete request
+app.delete("/api/courses/:id", (req, res) => {
+  //Look up the courses
+  //if not exist, return 404
+  const id = parseInt(req.params.id);
+  const course = courses.find((c) => c.id === id);
+  if (!course) return res.status(404).send(`Course not found`);
+  //delete the course
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
   res.send(course);
 });
 
